2145f078780fb311cb4a131c1f7229c2894d2a7b
skrollu <mat98-76@live.fr>
2023-05-20 16:30:10 +0200

    fix(web): use Authentication instead of Principal

--- backend/src/main/java/com/wenabi/interview/web/resource/WishResource.java
+++ backend/src/main/java/com/wenabi/interview/web/resource/WishResource.java
@@ -12,11 +12,11 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.security.Principal;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -30,8 +30,8 @@ public class WishResource {
 
     @PreAuthorize("hasRole('ASSOCIATION')")
     @GetMapping
-    ResponseEntity<Page<WishOutputDto>> getWishesByPageAndSortedByStatus(@PageableDefault(size = 10, page = 0, direction = Sort.Direction.ASC, sort = "status") Pageable pageable, Principal principal) {
-        UserJpa user = (UserJpa) principal;
+    ResponseEntity<Page<WishOutputDto>> getWishesByPageAndSortedByStatus(@PageableDefault(size = 10, page = 0, direction = Sort.Direction.ASC, sort = "status") Pageable pageable, Authentication principal) {
+        UserJpa user = (UserJpa) principal.getPrincipal();
         List<WishOutputDto> result = wishService.getWishesByPageAndUserId(pageable, user.getId()).stream().map(wishDtoMapper::mapToWishOutputDto).collect(Collectors.toList());
         return ResponseEntity.ok(new PageImpl<WishOutputDto>(result, pageable, pageable.getPageSize()));
     }
